4bd2ea7f76c4edfecab077468077ad81fd5a6dab
ajuanillo <ajuanillo@byupathway.edu>
2025-01-13 11:53:49 -0500

    Excercise 02

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -1,9 +1,61 @@
 using System;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Please, write your grade percentage: ");
+        int grade=int.Parse(Console.ReadLine());
+        string letterGrade="";
+        string gradeSymbol="";
+        if (grade>=90)
+        {
+            letterGrade="A";
+        }
+        else if (grade>=80)
+        {
+            letterGrade="B";
+        }
+        else if (grade>=70)
+        {
+            letterGrade="C";
+        }
+        else if (grade>=60)
+        {
+             letterGrade="D";
+        }
+        else 
+        {  
+            letterGrade="F";
+        }
+        if (grade>=60&&grade<97)
+        {
+            if ((grade%10)>=7)
+            {
+                gradeSymbol="+";
+            }
+            else if ((grade%10)<3)
+            {
+                gradeSymbol="-";
+            }
+            else
+            {
+                gradeSymbol="";
+            }
+        }
+        else
+        {
+            gradeSymbol="";
+        }
+        Console.WriteLine($"Your letter grade is {letterGrade}{gradeSymbol}");
+        if (grade>=70)
+        {
+            Console.WriteLine("Yo passed the curse!");
+        }
+        else
+        {
+            Console.WriteLine("You did not passed the curse, try again in next semester");
+        }
     }
 }
