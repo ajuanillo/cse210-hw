05e012e2ab1cd828992a52110771761c6d0c1bf4
ajuanillo <ajuanillo@byupathway.edu>
2025-01-13 19:25:27 -0500

    try again

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        Console.WriteLine("Hello to the program!");
+        Console.WriteLine("Please enter your name: ");
+        Console.WriteLine("Please enter your favorite number: ");
     }
 }
