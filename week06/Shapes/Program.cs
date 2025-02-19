223244323afeae3a5ab1ebfe366cc324e8052681
ajuanillo <ajuanillo@byupathway.edu>
2025-02-18 20:30:59 -0500

    Shape with polymorph

--- week06/Shapes/Program.cs
+++ week06/Shapes/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Shapes Project.");
+
+        Console.WriteLine("--  SQUARE  --");
+        Square square = new Square("Red", 5);
+
+        Console.WriteLine($"Square color: {square.GetColor()}");
+        Console.WriteLine($"Square area: {square.GetArea()}");
+
+        Console.WriteLine("--  RECTANGLE  --");
+        
+        Rectangle rectangle = new Rectangle("Red",4, 5);
+        Console.WriteLine($"Rectangle color: {rectangle.GetColor()}");
+        Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+        
+        Console.WriteLine("--  CIRCLE  --");
+        Circle circle = new Circle("Red",4);
+        Console.WriteLine($"Circle color: {circle.GetColor()}");
+        Console.WriteLine($"Circle area: {circle.GetArea()}");
+
+        List<Shape> shapes = new List<Shape>(){square,rectangle,circle};
+
+        Console.WriteLine("--  Looping Shape List  --");
+
+        foreach (Shape shape in shapes)
+        {
+            Console.WriteLine($"Shape color: {shape.GetColor()}");
+            Console.WriteLine($"Shape area: {shape.GetArea()}");
+        }
+
+
     }
 }
---
+++ week06/Shapes/Shape.cs
@@ -0,0 +1,16 @@
+class Shape
+{
+    protected string _color="";
+    public Shape(string color)
+    {
+        _color=color;
+    }
+    public string GetColor()
+    {
+        return _color;
+    }
+    public virtual double GetArea()
+    {
+        return 0;
+    }
+}
